2aa764785879215803d6a113574887b795cfc126
00sleng00 <roman.morshikoff@yandex.ru>
2022-06-18 21:59:06 +0300

    Проектная работа №15

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.roman.m.students.nomoreparties.sbs";
 
 const checkResponse = (res) => {
    if (res.ok) {
