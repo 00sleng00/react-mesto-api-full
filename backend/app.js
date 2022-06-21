5a7502f4c5b152c857a2678243d335add8824446
00sleng00 <roman.morshikoff@yandex.ru>
2022-06-21 22:30:25 +0300

    Проектная работа №15

--- backend/app.js
+++ backend/app.js
@@ -3,24 +3,21 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const { errors, celebrate, Joi } = require('celebrate');
-const cors = require('./middlewares/cors');
-
+const cors = require('cors');
 require('dotenv').config();
+const { errors, celebrate, Joi } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const { userRoutes } = require('./routes/users');
 const { cardRoutes } = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(cors);
 app.use(cookieParser());
 
 const limiter = rateLimit({
@@ -30,16 +27,19 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use(requestLogger);
-app.use(limiter);
+app.use(cors());
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+app.use(limiter);
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
@@ -66,8 +66,6 @@ app.use('/cards', auth, cardRoutes);
 app.all('*', auth, (_req, _res, next) => {
   next(new NotFoundError('Страница не  найдена'));
 });
-
-app.use(errorLogger);
 
 app.use(errors());
 
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.roman.m.students.nomoreparties.sbs";
+export const BASE_URL = "roman.m.students.nomoredomains.xyz";
 
 const checkResponse = (res) => {
    if (res.ok) {
